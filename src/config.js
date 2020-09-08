1cbc4119bf68dab96b1676fbdbbb126bad3a4692
Brunno Vodola <bvodola@gmail.com>
2020-09-08 17:07:17 -0300

    Changing URL

--- src/config.js
+++ src/config.js
@@ -2,11 +2,11 @@ const config = {
   BACKEND_URL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : "https://intense-thicket-75118.herokuapp.com",
+      : "https://sistema-fgm.herokuapp.com",
   SYSTEM_URL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:4000"
-      : "https://sistema.fgmdasorte.com.br"
+      : "https://sistema.fgmdasorte.com.br",
 };
 
 export default config;
